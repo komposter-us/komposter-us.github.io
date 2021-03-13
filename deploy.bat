b0bfd71a22725a131845fc131c0e48473b6a1892
raziEiL <mr.raz4291@gmail.com>
2021-03-13 14:10:27 +0500

    update script

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,12 @@
 :: just to get us all at the same starting point
 git checkout master
 
-:: create a 'dist' directory checked out to the gh-pages branch
-git worktree add -B gh-pages dist origin/gh-pages
-
 :: buld the project
 npm run build
 
-:: cd into 'dist' folder, which is now on the gh-pages branch
-cd dist
-
-:: fail if for some reason this isn't the gh-pages branch
-current_branch=$(git symbolic-ref --short -q HEAD)
-if [ "$current_branch" != "gh-pages" ]; then
-  echo "Expected dist folder to be on gh-pages branch."
-  exit 1
-fi
-
 :: commit and push to gh-pages
-git add . && git commit -m "Update gh-pages"
-git push
+git add dist
+git commit -m "Update gh-pages"
+git subtree push --prefix dist origin gh-pages
+pause
+
