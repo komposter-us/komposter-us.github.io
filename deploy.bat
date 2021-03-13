8c8cc2b8fd485601afdc074a52bc850e9fcfe09f
raziEiL <mr.raz4291@gmail.com>
2021-03-13 13:59:45 +0500

    Update gh-pages

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,22 @@
-# just to get us all at the same starting point
+:: just to get us all at the same starting point
 git checkout master
 
-# delete your dist folder
-rm -rf dist
-
-# create a 'dist' directory checked out to the gh-pages branch
+:: create a 'dist' directory checked out to the gh-pages branch
 git worktree add -B gh-pages dist origin/gh-pages
 
-# dist the project
-bundle exec rake dist
+:: buld the project
+npm run build
 
-# cd into 'dist' folder, which is now on the gh-pages branch
+:: cd into 'dist' folder, which is now on the gh-pages branch
 cd dist
+
+:: fail if for some reason this isn't the gh-pages branch
+current_branch=$(git symbolic-ref --short -q HEAD)
+if [ "$current_branch" != "gh-pages" ]; then
+  echo "Expected dist folder to be on gh-pages branch."
+  exit 1
+fi
+
+:: commit and push to gh-pages
+git add . && git commit -m "Update gh-pages"
+git push
