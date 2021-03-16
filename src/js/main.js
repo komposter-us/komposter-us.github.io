5a7d36bf14019129cde40523db35e38257431b70
raziEiL <mr.raz4291@gmail.com>
2021-03-16 16:03:02 +0500

    feat: remove overlay

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@
 | Defines
 |--------------------------------------------------------------------------
 */
+const UPDATE_OVERLAYED_CARD_STATUS = false;
+
 // indexes in phrases[lang].statusFull & phrases[lang].statusShort
 const AliveStatus = {
     Unknown: 0,
@@ -18,7 +20,7 @@ const Misc = {
     Round: 0
 };
 
-const phrases = {
+const g_oPhrases = {
     "ru": {
         misc: ["Раунд"],
         statusShort: ["?", "К", "Ч", "М", "И", "У", "В"],
@@ -31,24 +33,24 @@ const phrases = {
     }
 };
 
-const crewmateColor = ["#C51111", "#132ED1", "#117F2D", "#ED54BA", "#EF7D0E", "#F6F658", "#3F474E", "#D6E0F0", "#6B31BC", "#71491E", "#38FEDB", "#50EF39"];
-const lang = window.navigator.language.slice(0, 2) === "ru" ? "ru" : "en";
-const getCssOverlayClass = (aliveStatus) => "overlay-" + phrases.en.statusFull[aliveStatus].toLowerCase();
+const g_aCrewmateColor = ["#C51111", "#132ED1", "#117F2D", "#ED54BA", "#EF7D0E", "#F6F658", "#3F474E", "#D6E0F0", "#6B31BC", "#71491E", "#38FEDB", "#50EF39"];
+const g_sLang = window.navigator.language.slice(0, 2) === "ru" ? "ru" : "en";
+const getCssOverlayClass = (aliveStatus) => "overlay-" + g_oPhrases.en.statusFull[aliveStatus].toLowerCase();
 /*
 |--------------------------------------------------------------------------
 | Round Control
 |--------------------------------------------------------------------------
 */
-let round = 1;
+let g_iRound = 1;
 
 const btnRound = document.querySelector(".btn-round");
 const btnText = document.querySelector(".btn-round-num");
 const btnRoundText = document.querySelector(".btn-round-text");
-btnRoundText.textContent = phrases[lang].misc[Misc.Round];
+btnRoundText.textContent = g_oPhrases[g_sLang].misc[Misc.Round];
 
 btnRound.addEventListener("click", function () {
-    btnText.textContent = ++round;
-    updateRoundCards();
+    btnText.textContent = ++g_iRound;
+    onRoundBtnClick();
 });
 
 const btnRestart = document.querySelector(".btn-restart");
@@ -66,20 +68,20 @@ createCards();
 function createCards() {
     const cardContainter = document.querySelector(".card-containter");
 
-    for (const [i, color] of crewmateColor.entries()) {
-        const cardRounds = document.createElement("div");
-        cardRounds.classList.add("card-rounds");
-        createRoundCard(cardRounds, AliveStatus.Unknown);
+    for (const [i, color] of g_aCrewmateColor.entries()) {
+        const statusContainer = document.createElement("div");
+        statusContainer.classList.add("card-status-container");
+        createCardStatus(statusContainer, AliveStatus.Unknown);
 
-        const cardStatus = document.createElement("div");
-        cardStatus.classList.add("card-status");
+        const btnContainer = document.createElement("div");
+        btnContainer.classList.add("card-btn-container");
 
-        for (let aliveStatus = 0; aliveStatus < phrases[lang].statusFull.length; aliveStatus++) {
+        for (let aliveStatus = 0; aliveStatus < g_oPhrases[g_sLang].statusFull.length; aliveStatus++) {
             const btn = document.createElement("button");
             btn.classList.add("btn-status");
-            btn.append(document.createTextNode(phrases[lang].statusFull[aliveStatus]));
-            cardStatus.append(btn);
-            btn.addEventListener("click", btnStatusCallback.bind(btn, i, aliveStatus));
+            btn.append(document.createTextNode(g_oPhrases[g_sLang].statusFull[aliveStatus]));
+            btnContainer.append(btn);
+            btn.addEventListener("click", onStatusBtnClick.bind(btn, i, aliveStatus));
         }
 
         const card = document.createElement("div");
@@ -91,8 +93,8 @@ function createCards() {
         btn.addEventListener("click", closeCard);
 
         card.append(btn);
-        card.append(cardRounds);
-        card.append(cardStatus);
+        card.append(statusContainer);
+        card.append(btnContainer);
 
         cardContainter.append(card);
     }
@@ -104,54 +106,93 @@ function closeCard() {
 }
 /*
 |--------------------------------------------------------------------------
-| Round Card Logic
+| Card Logic
 |--------------------------------------------------------------------------
 */
 /** @this {HTMLButtonElement} */
-function btnStatusCallback(cardNum, aliveStatus) {
+function onStatusBtnClick(cardNum, aliveStatus) {
     /** @type {HTMLDivElement} */
     const card = this.parentElement.parentElement;
     /** @type {HTMLDivElement} */
-    const cardRounds = card.querySelector(".card-rounds");
-    const statusCount = cardRounds.querySelectorAll("*").length;
+    const statusContainer = card.querySelector(".card-status-container");
+    const statusNums = statusContainer.querySelectorAll("*").length;
 
-    console.log(`Card: ${cardNum}, status: ${aliveStatus}, statusCount: ${statusCount}`);
+    console.log(`onStatusBtnClick > Card: ${cardNum}, status: ${aliveStatus}, statusNums: ${statusNums}`);
 
-    if (statusCount < round)
-        createRoundCard(cardRounds, aliveStatus);
+    if (statusNums < g_iRound)
+        createCardStatus(statusContainer, aliveStatus);
     else
-        updateRoundCardStatus(cardRounds.lastElementChild, aliveStatus);
+        updateCardStatus(statusContainer.lastElementChild, aliveStatus);
 
     if (aliveStatus > AliveStatus.Suspect) {
+        console.log(`onStatusBtnClick > Card: ${cardNum} add overlay`);
         card.classList.add(getCssOverlayClass(aliveStatus));
         card.querySelector(".btn-close").classList.add("btn-close-white");
+
+        setTimeout(() => {
+            card.addEventListener("click", () => {
+                console.log(`onStatusBtnClick > Card: ${cardNum} Remove overlay`);
+                card.classList.remove(getCssOverlayClass(aliveStatus));
+                card.querySelector(".btn-close").classList.remove("btn-close-white");
+                setStatusContainerChildsVis(statusContainer, false);
+                updateStatusContainer(statusContainer);
+            }, { once: true });
+        }, 500);
     }
 }
 
-function createRoundCard(rootElem, aliveStatus) {
-    const elem = document.createElement("div");
-    elem.classList.add("card-round");
-    elem.dataset.status = aliveStatus;
-    updateRoundCardStatus(elem, aliveStatus);
-    rootElem.append(elem);
+/** @param {HTMLDivElement} rootElem */
+function createCardStatus(rootElem, aliveStatus, invis = false) {
+    const cardStatus = document.createElement("div");
+    cardStatus.classList.add("card-status");
+    cardStatus.dataset.status = aliveStatus;
+    updateCardStatus(cardStatus, aliveStatus);
+    rootElem.append(cardStatus);
+
+    if (invis)
+        cardStatus.classList.add("invisible");
 }
 
-function updateRoundCardStatus(elem, aliveStatus) {
-    elem.innerHTML = phrases[lang].statusShort[aliveStatus];
-    elem.dataset.status = aliveStatus;
-    console.log(elem);
+/** @param {HTMLDivElement} cardStatus */
+function updateCardStatus(cardStatus, aliveStatus) {
+    cardStatus.innerHTML = g_oPhrases[g_sLang].statusShort[aliveStatus];
+    cardStatus.dataset.status = aliveStatus;
+    console.log("updateCardStatus >", cardStatus);
 }
 
-function updateRoundCards() {
+function onRoundBtnClick() {
     /** @type {HTMLDivElement[]} */
-    const cards = document.querySelectorAll(".card-rounds");
+    const statusContainers = document.querySelectorAll(".card-status-container");
+    let hasOverlay;
 
-    for (const card of cards) {
-        // console.log(card.lastElementChild, card.lastElementChild.dataset.status);
-        if (card.lastElementChild.dataset.status > AliveStatus.Suspect)
+    for (const cardStatus of statusContainers) {
+        hasOverlay = cardStatus.lastElementChild.dataset.status > AliveStatus.Suspect || cardStatus.lastElementChild.classList.contains("invisible");
+
+        if (!UPDATE_OVERLAYED_CARD_STATUS && hasOverlay)
             continue;
 
-        if (card.querySelectorAll("*").length < round)
-            createRoundCard(card, AliveStatus.Unknown);
+        if (cardStatus.querySelectorAll("*").length < g_iRound)
+            createCardStatus(cardStatus, AliveStatus.Unknown, UPDATE_OVERLAYED_CARD_STATUS && hasOverlay);
     }
 }
+
+/** @param {HTMLDivElement} statusContainer */
+function updateStatusContainer(statusContainer) {
+    let nums = statusContainer.querySelectorAll("*").length;
+
+    while (nums++ < g_iRound)
+        createCardStatus(statusContainer, AliveStatus.Unknown);
+}
+
+/** @param {HTMLDivElement} statusContainer */
+function setStatusContainerChildsVis(statusContainer, hide) {
+    const cardStatuses = statusContainer.querySelectorAll("*");
+
+    for (const cardStatus of cardStatuses) {
+        if (hide)
+            cardStatus.classList.add("invisible");
+        else
+            cardStatus.classList.remove("invisible");
+    }
+}
+//
