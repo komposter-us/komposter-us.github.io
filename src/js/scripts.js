fdc5df47311e1c041b5e5781ddd70633f9d7ce4c
raziEiL <mr.raz4291@gmail.com>
2021-06-21 03:27:12 +0500

    Feat: show card & reset cards

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -58,124 +58,111 @@ btnRoundText.textContent = g_oPhrases[g_sLang].misc[Misc.Round];
 btnRound.addEventListener("click", onRoundBtnClick);
 
 const btnRestart = document.querySelector(".btn-restart");
-btnRestart.addEventListener("click", function () {
-    document.location.reload();
-});
-
+btnRestart.addEventListener("click", onResetBtnClick);
 /*
 |--------------------------------------------------------------------------
 | Create Cards
 |--------------------------------------------------------------------------
 */
-createCards();
+const cardContainter = document.querySelector(".card-containter");
+const colorContainter = document.querySelector(".color-container");
 
-function createCards() {
-    const cardContainter = document.querySelector(".card-containter");
-    const colorContainter = document.querySelector(".color-container");
+for (const [cardNum, color] of g_aCrewmateColor.entries()) {
+    // cards
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.style.backgroundColor = color;
+    card.dataset.status = AliveStatus.Unknown;
+    card.dataset.round = 0;
 
-    for (const [cardNum, color] of g_aCrewmateColor.entries()) {
-        // cards
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.style.backgroundColor = color;
-        card.dataset.status = AliveStatus.Unknown;
-        card.dataset.round = 0;
+    const text = document.createElement("h6");
+    if (cardNum > 6)
+        text.style.color = "#000";
 
-        const text = document.createElement("h6");
-        if (cardNum > 6)
-            text.style.color = "#000";
+    const statusContainer = document.createElement("div");
+    statusContainer.classList.add("card-status-container", "card-container-indent");
 
-        const statusContainer = document.createElement("div");
-        statusContainer.classList.add("card-status-container", "card-container-indent");
-
-        for (let i = 0; i < MAX_ROUND; i++) {
-            const cardStatus = document.createElement("div");
-            cardStatus.classList.add("card-status");
-            statusContainer.append(cardStatus);
-            cardStatus.addEventListener("mouseenter", onMouseEnterStatus.bind(text, cardStatus, i + 1));
-            cardStatus.addEventListener("mouseout", onMouseOverStatus.bind(text));
-            cardStatus.addEventListener("click", onClickStatus.bind(cardStatus, card, i));
-            updateCardStatus(cardStatus, i ? AliveStatus.Empty : AliveStatus.Unknown);
-        }
-        statusContainer.firstChild.classList.add("card-status-active");
-
-        const btnContainer = document.createElement("div");
-        btnContainer.classList.add("btn-group", "card-container-indent");
-        btnContainer.setAttribute("role", "group");
-        btnContainer.setAttribute("aria-label", "Round status card " + cardNum);
-
-        const verdictContainer = document.createElement("div");
-        verdictContainer.classList.add("card-verdict-container", "card-container-indent");
-
-        for (let status = AliveStatus.Defended; status <= AliveStatus.Ejected; status++) {
-            let elem;
-            const bStatus = status > AliveStatus.Suspect;
-            if (bStatus) {
-                elem = document.createElement("button");
-                elem.classList.add("btn-verdict", "btn-" + getCssOverlayClass(status));
-                verdictContainer.append(elem);
-            }
-            else {
-                elem = document.createElement("input");
-                elem.classList.add("btn-check");
-                elem.setAttribute("type", "radio");
-                elem.setAttribute("name", `btnradio-${cardNum}`);
-                elem.setAttribute("id", `radio-${cardNum}-${status}`);
-                elem.setAttribute("autocomplete", "off");
-                elem.dataset.status = status;
-                if (status === AliveStatus.Unknown)
-                    elem.setAttribute("checked", "");
-                btnContainer.append(elem);
-
-                elem = document.createElement("label");
-                elem.classList.add("btn", "btn-status");
-                elem.setAttribute("for", `radio-${cardNum}-${status}`);
-                elem.append(document.createTextNode(g_oPhrases[g_sLang].status[status]));
-                // elem.dataset.status = status;
-                btnContainer.append(elem);
-            }
-            elem.addEventListener("click", onStatusBtnClick.bind(card, cardNum, status, bStatus));
-            elem.addEventListener("mouseenter", onMouseEnterBtn.bind(text, status, bStatus));
-            elem.addEventListener("mouseout", onMouseOverBtn.bind(text));
-        }
-
-        const controlContainer = document.createElement("div");
-        controlContainer.classList.add("card-control-container");
-
-        controlContainer.append(text);
-
-        // color control
-        const colorClose = document.createElement("div");
-        colorClose.classList.add("color-close");
-        colorClose.style.backgroundColor = color;
-        colorContainter.append(colorClose);
-        colorClose.addEventListener("click", onCloseBtnClick.bind(card));
-        colorClose.addEventListener("click", onColorClose);
-
-        const btn = document.createElement("button");
-        btn.classList.add("btn-close");
-        btn.addEventListener("click", onCloseBtnClick.bind(card));
-        btn.addEventListener("click", onColorClose.bind(colorClose));
-        controlContainer.append(btn);
-
-        const content = document.createElement("div");
-        content.classList.add("card-content-container", "card-container-indent");
-
-        content.append(statusContainer);
-        content.append(btnContainer);
-        content.append(verdictContainer);
-
-        card.append(controlContainer);
-        card.append(content);
-
-        cardContainter.append(card);
-        g_aCards.push(card);
+    for (let i = 0; i < MAX_ROUND; i++) {
+        const cardStatus = document.createElement("div");
+        cardStatus.classList.add("card-status");
+        statusContainer.append(cardStatus);
+        cardStatus.addEventListener("mouseenter", onMouseEnterStatus.bind(text, cardStatus, i + 1));
+        cardStatus.addEventListener("mouseout", onMouseOverStatus.bind(text));
+        cardStatus.addEventListener("click", onClickStatus.bind(cardStatus, card, i));
+        updateCardStatus(cardStatus, i ? AliveStatus.Empty : AliveStatus.Unknown);
     }
-}
+    statusContainer.firstChild.classList.add("card-status-active");
 
-/** @this {HTMLDivElement} */
-function onColorClose() {
-    this.classList.add("color-closed");
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btn-group", "card-container-indent");
+    btnContainer.setAttribute("role", "group");
+    btnContainer.setAttribute("aria-label", "Round status card " + cardNum);
+
+    const verdictContainer = document.createElement("div");
+    verdictContainer.classList.add("card-verdict-container", "card-container-indent");
+
+    for (let status = AliveStatus.Defended; status <= AliveStatus.Ejected; status++) {
+        let elem;
+        const bStatus = status > AliveStatus.Suspect;
+        if (bStatus) {
+            elem = document.createElement("button");
+            elem.classList.add("btn-verdict", "btn-" + getCssOverlayClass(status));
+            verdictContainer.append(elem);
+        }
+        else {
+            elem = document.createElement("input");
+            elem.classList.add("btn-check");
+            elem.setAttribute("type", "radio");
+            elem.setAttribute("name", `btnradio-${cardNum}`);
+            elem.setAttribute("id", `radio-${cardNum}-${status}`);
+            elem.setAttribute("autocomplete", "off");
+            elem.dataset.status = status;
+            if (status === AliveStatus.Unknown)
+                elem.setAttribute("checked", "");
+            btnContainer.append(elem);
+
+            elem = document.createElement("label");
+            elem.classList.add("btn", "btn-status");
+            elem.setAttribute("for", `radio-${cardNum}-${status}`);
+            elem.append(document.createTextNode(g_oPhrases[g_sLang].status[status]));
+            // elem.dataset.status = status;
+            btnContainer.append(elem);
+        }
+        elem.addEventListener("click", onStatusBtnClick.bind(card, cardNum, status, bStatus));
+        elem.addEventListener("mouseenter", onMouseEnterBtn.bind(text, status, bStatus));
+        elem.addEventListener("mouseout", onMouseOverBtn.bind(text));
+    }
+
+    const controlContainer = document.createElement("div");
+    controlContainer.classList.add("card-control-container");
+
+    controlContainer.append(text);
+
+    // color control
+    const colorClose = document.createElement("div");
+    colorClose.classList.add("color-close");
+    colorClose.style.backgroundColor = color;
+    colorContainter.append(colorClose);
+    const cardCloseCallback = onCardCloseBtnClick.bind(card, colorClose, cardNum);
+    colorClose.addEventListener("click", cardCloseCallback);
+
+    const btn = document.createElement("button");
+    btn.classList.add("btn-close");
+    btn.addEventListener("click", cardCloseCallback);
+    controlContainer.append(btn);
+
+    const content = document.createElement("div");
+    content.classList.add("card-content-container", "card-container-indent");
+
+    content.append(statusContainer);
+    content.append(btnContainer);
+    content.append(verdictContainer);
+
+    card.append(controlContainer);
+    card.append(content);
+
+    cardContainter.append(card);
+    g_aCards.push(card);
 }
 
 function onRoundBtnClick() {
@@ -190,13 +177,49 @@ function onRoundBtnClick() {
 }
 
 /** @this {HTMLDivElement} */
-function onCloseBtnClick() {
-    this.classList.add("opacity-0");
-    this.addEventListener("transitionend", function () {
-        this.classList.add("hidden");
-    });
+function onCardCloseBtnClick(colorClose) {
+    // show
+    if (colorClose.classList.contains("color-closed")) {
+        colorClose.classList.remove("color-closed", "hidden", "opacity-0");
+        this.classList.remove("hidden", "opacity-0");
+        this.removeEventListener("transitionend", onHide);
+    }
+    // hide
+    else {
+        colorClose.classList.add("color-closed");
+        this.classList.add("opacity-0");
+        this.addEventListener("transitionend", onHide);
+    }
+
 }
 
+function onHide() {
+    this.classList.add("hidden");
+}
+
+function onResetBtnClick() {
+    g_iRound = 1;
+    btnText.textContent = g_iRound;
+    // document.location.reload();
+    for (const card of g_aCards) {
+        // remove overlay
+        const content = card.querySelector(".card-content-container");
+        content.classList.remove(getCssOverlayClass(card.dataset.status));
+
+        // reset data/btns
+        card.dataset.round = 0;
+        card.dataset.status = AliveStatus.Unknown;
+        updateCardRound(card, AliveStatus.Unknown);
+        resetCardRadio(card);
+
+        // reset statuses
+        const cardStatuses = card.querySelectorAll(".card-status");
+
+        for (const [i, cardStatus] of cardStatuses.entries()) {
+            updateCardStatus(cardStatus, i ? AliveStatus.Empty : AliveStatus.Unknown);
+        }
+    }
+}
 /*
 |--------------------------------------------------------------------------
 | Card Logic
@@ -243,7 +266,6 @@ function onMouseOverBtn() {
 
 /** @this {HTMLTextElement} */
 function onMouseEnterStatus(cardStatus, round) {
-
     let phrase = getFullPhrase(cardStatus.dataset.status);
     phrase = phrase ? (": " + phrase) : "";
     this.innerHTML = g_oPhrases[g_sLang].misc[Misc.Round] + " " + round + phrase;
@@ -309,9 +331,10 @@ function updateCardRound(card, aliveStatus = AliveStatus.Empty, updateStatus = t
 function updateCardStatus(cardStatus, aliveStatus) {
     if (aliveStatus != AliveStatus.Empty)
         cardStatus.innerHTML = g_oPhrases[g_sLang].statusShort[aliveStatus];
+    else
+        cardStatus.innerHTML = "";
 
     cardStatus.dataset.status = aliveStatus;
-    // console.log(aliveStatus);
 }
 
 /** @param {HTMLDivElement} card */
@@ -326,3 +349,14 @@ function resetCardRadio(card) {
             e.click();
     }
 }
+/*
+|--------------------------------------------------------------------------
+| Sotrable
+|--------------------------------------------------------------------------
+*/
+/* const { Sortable } = require("@shopify/draggable");
+
+new Sortable(document.querySelector(".card-containter"), {
+    draggable: ".card",
+    delay: { mouse: 1000, drag: 1000, touch: 1000 }
+}); */
